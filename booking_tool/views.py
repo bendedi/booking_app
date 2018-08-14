ff4791fbc489a9bfbe88a3b149ed218819858c5a
MOIROUD <benjamin.moiroud@capgemini.com>
2018-08-14 15:57:43 +0200

    +

--- booking_tool/views.py
+++ booking_tool/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts	import render
 from django.template	import loader
 from django.http		import HttpResponse, HttpResponseRedirect
+from re					import match
+
+MIN = 2
+MAX = 80
 
 def		index(request):
 	# return (HttpResponse("salut"))
 	return (render(request, "booking_tool/index.html"))
 
 def		send(request):
-	pass
+	mail = request.POST['contact']
+	number = request.POST['number']
+	week = request.POST['week']				# forme: 20XX-WXX
+
 # Create your views here.
