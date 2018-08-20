c50a65c276350935cc9e601f502badf04a607e19
DUPONT DE DINECHIN <benoit.dupont-de-dinechin@capgemini.com>
2018-08-20 09:46:06 +0200

    partie 1 formulaire

--- booking_tool/static/script_personnalise.js
+++ booking_tool/static/script_personnalise.js
@@ -2,3 +2,34 @@ function updateNumber(nbPersonne) {
     toShow= document.getElementById("showNumber");
     toShow.innerHTML=nbPersonne
 }
+function hiddeDayTime() {
+    containerHidding= document.getElementById("toshow")
+    containerHidding.innerHTML= ""
+    //containerStyle= document.getElementById("sub")
+    //containerStyle.style.marginTop = "10vh";
+}
+
+function showDayTime() {
+    var user = 1 ;
+    containerShow= document.getElementById("toshow")
+    containerShow.innerHTML= `<div class="form-check">
+                <input class="plage" type="radio" name="exampleRadios1">
+                <label class="form-check-label" for="exampleRadios2">
+                Morning
+                </label>
+                </div>
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios1" >
+                <label class="form-check-label" for="exampleRadios2">
+                Afternoon
+                </label>
+                </div>
+                <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios1" >
+                <label class="form-check-label" for="exampleRadios2">
+                Whatever
+                </label>
+                </div>`
+    //containerStyle= document.getElementById("sub")
+    //containerStyle.style.marginTop = "0%";
+}
