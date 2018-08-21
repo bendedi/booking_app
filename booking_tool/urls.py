135351796efb10ab529d8303f7c42301f65cfabf
MOIROUD <benjamin.moiroud@capgemini.com>
2018-08-21 09:48:07 +0200

    urls

--- booking_tool/urls.py
+++ booking_tool/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	re_path(r'^$', views.index, name='index'),
 	re_path(r'send/', views.send, name='send'),
 	re_path(r'^gettoken/', views.gettoken, name='gettoken'),
-	re_path(r'^mail/', views.mail, name='mail')
+	re_path(r'^mail/', views.mail, name='mail'),
+	re_path(r'^get_rooms/', views.get_room, name='get_room')
 ]
